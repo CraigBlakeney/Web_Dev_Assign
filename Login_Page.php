c4258dd3147a416a9e753aa73d72e6a725363913
Craig Blakeney <craigblakeney@gmail.com>
2018-11-26 13:26:24 +0000

    login

---
+++ Login_Page.php
@@ -0,0 +1,11 @@
+<?php include 'index.php'; ?>
+<html>
+	<section id="main";
+		<h1> Login </h1>
+			<form method="post">
+				<p>Username: <input type="text" name="username" required></p>
+				<p>Password: <input type="password" name="password" maxlength="6" required></p>
+				<p><input type="submit" value="Log In"></p>
+			</form>
+
+</html>
